d0a2a583b39759c0b273c87859d4fd771fae5e84
gsimsekfb <31007983+gsimsekfb@users.noreply.github.com>
2017-08-28 09:26:42 +0300

    Reverting fake unit test failure

--- src/core/string_parser_test.cpp
+++ src/core/string_parser_test.cpp
@@ -13,7 +13,7 @@
 // (test_case_name, test_name) 
 TEST(StringParserTests, NumberLeftOf_StringWithoutNumber_ReturnsFalse)
 {
-    EXPECT_EQ(0, 42);
+    EXPECT_EQ(0, 0);
 }
 
 TEST(StringParserTests, SomeTest)
