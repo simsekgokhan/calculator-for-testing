30b06384b47316b5d772e057ec221bf019e8e3f5
gsimsekfb <31007983+gsimsekfb@users.noreply.github.com>
2018-12-30 09:30:21 +0300

    Adding compare_int function for testing

--- src/core/string_parser.h
+++ src/core/string_parser.h
@@ -4,6 +4,9 @@
 
 struct StringParser
 {
+    // Function for testing only
+    static bool compare_int(int x, int y) { return x == y; }
+
     static int numberLeftOf(std::string const& str, std::string::const_iterator pos);
     static int numberRightOf(std::string const& str, std::string::const_iterator pos);
 };
